99dc0c3a9e3c6cf32fc04125d43fbd46076842c5
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-07-05 03:01:31 +0300

    update UserDetail

--- components/User/userDetail.tsx
+++ components/User/userDetail.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
+import Image from "next/image";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { IUserAuth } from "../../redux/auth/authReducer";
 import { useAppDispatch } from "../../redux/hooks";
 import { getUserById } from "../../redux/moderate/moderateOperations";
 import { getUserSelect } from "../../redux/moderate/moderateSelector";
+import styles from "../../styles/components/User/UserDetail.module.css";
 
 interface UserDetailProps {
   userId: string;
@@ -28,7 +30,27 @@ const UserDetail: React.FC<UserDetailProps> = ({ userId }) => {
 
   return (
     <div>
+      <div className={styles.imageWrapper}>
+        <Image
+          src={user.avatarURL}
+          alt="avatar"
+          width={150}
+          height={200}
+          style={{
+            objectFit: "cover",
+            margin: "auto",
+          }}
+        />
+      </div>
       <p>{user.firstName}</p>
+      <p>{user.lastName}</p>
+      <p>{user.email}</p>
+      <p>{user.isOnline}</p>
+      <p>{user.location}</p>
+      <p>{user.phone}</p>
+      <p>{user.role}</p>
+      <p>{user.createdAt}</p>
+      <p>список обьявлений</p>
     </div>
   );
 };
