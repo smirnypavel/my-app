1815ef4b80fe9fd49735a1511cc3b616db134cad
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-07-05 18:54:53 +0300

    add admin change role

--- components/User/userDetail.tsx
+++ components/User/userDetail.tsx
@@ -1,12 +1,13 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { IUserAuth } from "../../redux/auth/authReducer";
 import { useAppDispatch } from "../../redux/hooks";
 import { getUserById } from "../../redux/moderate/moderateOperations";
 import { getUserSelect } from "../../redux/moderate/moderateSelector";
 import styles from "../../styles/components/User/UserDetail.module.css";
+import { getRole } from "../../redux/auth/authSelectors";
 
 interface UserDetailProps {
   userId: string;
@@ -17,6 +18,12 @@ const UserDetail: React.FC<UserDetailProps> = ({ userId }) => {
   const { id } = router.query;
   const dispatch = useAppDispatch();
   const user: IUserAuth = useSelector(getUserSelect);
+  const role = useSelector(getRole);
+  const [selectedRole, setSelectedRole] = useState<string>(user.role);
+
+  const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedRole(event.target.value);
+  };
 
   useEffect(() => {
     try {
@@ -33,29 +40,39 @@ const UserDetail: React.FC<UserDetailProps> = ({ userId }) => {
   }
 
   return (
-    <div>
-      <div className={styles.imageWrapper}>
-        <Image
-          src={user.avatarURL}
-          alt="avatar"
-          width={150}
-          height={200}
-          style={{
-            objectFit: "cover",
-            margin: "auto",
-          }}
-        />
+    <>
+      <div>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={user.avatarURL}
+            alt="avatar"
+            width={150}
+            height={200}
+            style={{
+              objectFit: "cover",
+              margin: "auto",
+            }}
+          />
+        </div>
+        <p>{user.firstName}</p>
+        <p>{user.lastName}</p>
+        <p>{user.email}</p>
+        <p>{user.isOnline}</p>
+        <p>{user.location}</p>
+        <p>{user.phone}</p>
+        <p>{user.role}</p>
+        <p>{user.createdAt}</p>
+        {role === "admin" && (
+          <select
+            value={selectedRole}
+            onChange={handleRoleChange}>
+            <option value="user">User</option>
+            <option value="moderator">Moderator</option>
+          </select>
+        )}
+        <p>список обьявлений</p>
       </div>
-      <p>{user.firstName}</p>
-      <p>{user.lastName}</p>
-      <p>{user.email}</p>
-      <p>{user.isOnline}</p>
-      <p>{user.location}</p>
-      <p>{user.phone}</p>
-      <p>{user.role}</p>
-      <p>{user.createdAt}</p>
-      <p>список обьявлений</p>
-    </div>
+    </>
   );
 };
 
