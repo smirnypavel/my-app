0c9d687f8219f47622137622e00ece928fbbefbc
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-07-09 13:16:55 +0300

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "../styles/Page/404.module.css";
+import Image from "next/image";
+import Layout from "../components/Layout/Layout";
+import Link from "next/link";
+
+const Error404 = () => {
+  return (
+    <div className={styles.oopss}>
+      <Layout>
+        <div className={styles.errorText}>
+          <Image
+            src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
+            alt="404"
+            width={200}
+            height={200}
+          />
+          <span>404 PAGE</span>
+          <p className={styles.pA}>
+            . The page you were looking for could not be found
+          </p>
+          <p className={styles.pB}>... Back to previous page</p>
+          <Link
+            href="/"
+            className={styles.back}>
+            ... Back to previous page
+          </Link>
+        </div>
+      </Layout>
+    </div>
+  );
+};
+
+export default Error404;
