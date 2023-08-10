831f9139b637162a96bb32a332da1a3ff9af53f2
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-08-10 16:02:16 +0300

    mobile styles commit

--- components/Account/AcountPage.tsx
+++ components/Account/AcountPage.tsx
@@ -3,29 +3,19 @@ import { logOut } from "../../redux/auth/authOperations";
 import { useAppDispatch } from "../../redux/hooks";
 import { useRouter } from "next/router";
 import styles from "../../styles/Page/Account.module.css";
-import Profile from "./Profile";
 
-import UserProduct from "./UserProduct";
-import MyFavorites from "./MyFavorites";
-import MeExchangeList from "./ProductExchange/MeExchangeList";
-import IExchangeList from "./ProductExchange/IExchangeList";
-import MyDeals from "./ProductExchange/MyDeals";
+import { MdMenu, MdClear } from "react-icons/md";
 
 import { HiMiniArrowLeftOnRectangle } from "react-icons/hi2";
 import Link from "next/link";
 
-enum ActiveComponent {
-  PROFILE = "profile",
-  FAVORITES = "MyFavorites",
-  MyPRODUCT = "UserProduct",
-  MeOFFER = "MeExchangeList",
-  MyOFFER = "IExchangeList",
-  MyDEALS = "MyDeals",
-}
-
 const AccountPage: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen); // Изменение состояния меню при нажатии
+  };
 
   const isActiveLink = (path: string): boolean => {
     return router.pathname === path;
@@ -39,6 +29,75 @@ const AccountPage: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
       <div className={styles.accountContainer}>
+        {isMenuOpen && (
+          <div
+            className={styles.modalBackdrop}
+            onClick={handleMenuToggle}></div>
+        )}
+
+        <div
+          className={`${styles.mobileButtonContainer} ${
+            isMenuOpen ? styles.open : ""
+          }`}>
+          <button
+            onClick={handleMenuToggle}
+            className={styles.menuButton}>
+            <MdClear />
+          </button>
+          <Link
+            href="/account"
+            className={`${styles.button} ${
+              isActiveLink("/account") ? styles.activeButton : ""
+            }`}>
+            Profile
+          </Link>
+          <Link
+            href="/account/favorites"
+            className={`${styles.button} ${
+              isActiveLink("/account/favorites") ? styles.activeButton : ""
+            }`}>
+            Favorites
+          </Link>
+          <Link
+            className={`${styles.button} ${
+              isActiveLink("/account/my-product") ? styles.activeButton : ""
+            }`}
+            href="/account/my-product">
+            My product
+          </Link>
+          <Link
+            className={`${styles.button} ${
+              isActiveLink("/account/i-offered") ? styles.activeButton : ""
+            }`}
+            href="/account/i-offered">
+            I offered
+          </Link>
+          <Link
+            className={`${styles.button} ${
+              isActiveLink("/account/me-offer") ? styles.activeButton : ""
+            }`}
+            href="/account/me-offer">
+            Me offer
+          </Link>
+          <Link
+            className={`${styles.button} ${
+              isActiveLink("/account/my-deals") ? styles.activeButton : ""
+            }`}
+            href="/account/my-deals">
+            My deals
+          </Link>
+          <button
+            onClick={handleLogOut}
+            className={styles.logOutButton}>
+            <HiMiniArrowLeftOnRectangle className={styles.icon} /> LogOut
+          </button>
+        </div>
+        <div>
+          <MdMenu
+            className={styles.burgerMenu}
+            onClick={handleMenuToggle}
+          />
+        </div>
         <div className={styles.buttonContainer}>
           <Link
             href="/account"
@@ -93,6 +152,7 @@ const AccountPage: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             <HiMiniArrowLeftOnRectangle className={styles.icon} /> LogOut
           </button>
         </div>
+
         {children}
       </div>
     </>
